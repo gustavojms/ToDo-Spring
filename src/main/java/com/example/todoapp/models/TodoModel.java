17c61ea6496e95e204cfa2ccfb49d3b704e6ef57
gustavojms <gustavo.joset0@hotmail.com>
2023-02-08 22:49:58 -0300

    Creating model

---
+++ src/main/java/com/example/todoapp/models/TodoModel.java
@@ -0,0 +1,33 @@
+package com.example.todoapp.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_TODO")
+public class TodoModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "done", nullable = false)
+    private boolean done;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
