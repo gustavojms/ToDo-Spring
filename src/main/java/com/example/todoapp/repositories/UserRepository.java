35ecd5b7ca9d4c07a2a97d25cb5f83c0ecddc506
gustavojms <gustavo.joset0@hotmail.com>
2023-02-09 16:40:16 -0300

    Creating user repository

---
+++ src/main/java/com/example/todoapp/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.todoapp.repositories;
+
+import com.example.todoapp.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
