64c8317b81b3cc9cc18dc295993ca168747eb9e1
gustavojms <gustavo.joset0@hotmail.com>
2023-02-10 20:50:11 -0300

    Chaing generationtype from auto to identity

--- src/main/java/com/example/todoapp/models/TodoModel.java
+++ src/main/java/com/example/todoapp/models/TodoModel.java
@@ -15,7 +15,7 @@ public class TodoModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "title", nullable = false)
